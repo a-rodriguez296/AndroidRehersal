383c24429a41e652e7f954e3c765ce6fc995f6bf
alejo80805 <alejo80805@hotmail.com>
2015-09-01 21:58:03 -0500

    se creo la web view

---
+++ app/src/main/java/arf/com/baccus/controller/WebActivity.java
@@ -0,0 +1,76 @@
+package arf.com.baccus.controller;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import arf.com.baccus.R;
+import arf.com.baccus.model.Wine;
+
+/**
+ * Created by arodriguez on 9/1/15.
+ */
+public class WebActivity extends Activity {
+
+
+    //Vistas
+    private WebView mBrowser = null;
+    private ProgressBar mProgressBar = null;
+
+
+    //Modelo
+    private Wine mWine = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_web);
+
+        mWine = new Wine("Vegaval","Tinto",R.drawable.logo_facebook,"Casillero del diablo", "http://eltiempo.com/","adsfasdfadsf1","Valdepeñas",3);
+
+
+        mBrowser = (WebView) findViewById(R.id.browser);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        mBrowser.setWebViewClient(new WebViewClient(){
+
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                mProgressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+
+                mProgressBar.setVisibility(View.GONE);
+            }
+        });
+
+        mBrowser.getSettings().setJavaScriptEnabled(true);
+        mBrowser.getSettings().setBuiltInZoomControls(true);
+
+
+        mBrowser.loadUrl(mWine.getCompanyWeb());
+
+    }
+}
